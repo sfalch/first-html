cd595646e7e48971ef5cc0adc3ed6627b44a60d8
123itismesara <sfalch24@student.aau.dk>
2025-03-04 22:38:05 +0100

    valentine btn

--- first_website/main.js
+++ first_website/main.js
@@ -12,3 +12,34 @@ function dice_roll(x) {
 
   dices.innerHTML = a;
 }
+
+function noBtn() {
+  //yes button changes
+  let myDiv1 = document.querySelector(".yes-btn");
+  let currWidth_yes = myDiv1.getBoundingClientRect().width;
+  myDiv1.style.width = currWidth_yes + 10 + "px";
+  let currHeight_yes = myDiv1.getBoundingClientRect().height;
+  myDiv1.style.height = currHeight_yes + 10 + "px";
+
+  //no button changes
+  let myDiv2 = document.querySelector(".no-btn");
+
+  let currWidth_no = myDiv2.getBoundingClientRect().width;
+  let currHeight_no = myDiv2.getBoundingClientRect().height;
+
+  myDiv2.style.width = currWidth_no - 10 + "px";
+  myDiv2.style.height = currHeight_no - 10 + "px";
+
+  //change text in no button
+  const text = document.getElementById("no-btn");
+  const array = ["Are you sure?", "please?", "reconsider.. :((", "stop:(("];
+
+  a = Math.floor(Math.random() * 4);
+  if (currWidth_no <= 10) {
+    myDiv2.style.width = 0 + "px";
+    myDiv2.style.height = 0 + "px";
+    text.innerHTML = " ";
+  } else {
+    text.innerHTML = array[a];
+  }
+}
