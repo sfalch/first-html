fcafb9cf262f98f6017ef9c34088b7c89c7c6951
123itismesara <sfalch24@student.aau.dk>
2025-03-05 09:12:47 +0100

    alerts fixed

--- first_website/main.js
+++ first_website/main.js
@@ -16,17 +16,24 @@ function dice_roll(x) {
 function noBtn() {
   //yes button changes
   let myDiv1 = document.querySelector(".yes-btn");
+  const yes_text = document.getElementById("yes-btn");
   let currWidth_yes = myDiv1.getBoundingClientRect().width;
-  myDiv1.style.width = currWidth_yes + 10 + "px";
   let currHeight_yes = myDiv1.getBoundingClientRect().height;
-  myDiv1.style.height = currHeight_yes + 10 + "px";
+  if (currWidth_yes >= 400) {
+    yes_text.innerHTML = "Ok :(";
+  } else {
+    myDiv1.style.width = currWidth_yes + 10 + "px";
+    myDiv1.style.height = currHeight_yes + 10 + "px";
+  }
+
+  console.log("yes:" + currWidth_yes);
 
   //no button changes
   let myDiv2 = document.querySelector(".no-btn");
 
   let currWidth_no = myDiv2.getBoundingClientRect().width;
   let currHeight_no = myDiv2.getBoundingClientRect().height;
-  console.log(currWidth_no);
+  console.log("no:" + currWidth_no);
 
   myDiv2.style.width = currWidth_no - 10 + "px";
   myDiv2.style.height = currHeight_no - 10 + "px";
@@ -35,7 +42,24 @@ function noBtn() {
 
   if (currWidth_no <= 110) {
     document.getElementById("no-btn").style.fontSize = "100px";
-    text.innerHTML = " ";
+    text.innerHTML = "";
+  }
+
+  const array = [
+    "Are you sure?",
+    "please?",
+    "reconsider.. :((",
+    "stop:((",
+    "don't do this!",
+  ];
+
+  a = Math.floor(Math.random() * 5);
+
+  if (currWidth_yes >= 200 && currWidth_yes < 400) {
+    return alert(array[a]);
+  }
+  if (currWidth_yes >= 400) {
+    return alert("Question successfully declined ://");
   }
 
   //change text in no button
