6537032ac847732675cf294df907025b7039b9b3
123itismesara <sfalch24@student.aau.dk>
2025-03-20 20:07:59 +0100

    calculator added

--- first_website/main.js
+++ first_website/main.js
@@ -583,3 +583,83 @@ function check_palindrome() {
       input_text + " is NOT a palindrome of " + reverse + "!";
   }
 }
+
+//--------------------CALCULATOR------------------------------
+//https://www.geeksforgeeks.org/javascript-calculator/
+
+let currentInput = "";
+let currentOperation = "";
+let previousInput = "";
+
+let display = document.getElementById("calc_display");
+
+function appendNumber(number) {
+  currentInput += number;
+  display.textContent = `${previousInput} ${currentOperation} ${currentInput}`;
+
+  console.log(previousInput);
+  console.log(currentOperation);
+  console.log(currentInput);
+}
+
+function calculate() {
+  if (previousInput === "" || currentInput === "") return;
+  let result;
+  let prev = parseFloat(previousInput);
+  let current = parseFloat(currentInput);
+
+  switch (currentOperation) {
+    case "+":
+      result = prev + current;
+      break;
+    case "-":
+      result = prev - current;
+      break;
+    case "*":
+      result = prev * current;
+      break;
+    case "/":
+      if (current === 0) {
+        alert("Cannot divide by zero");
+        return;
+      }
+      result = prev / current;
+      break;
+    default:
+      return;
+  }
+
+  console.log(previousInput);
+  console.log(currentOperation);
+  console.log(currentInput);
+  console.log(result);
+  console.log(prev);
+  console.log(current);
+
+  currentInput = result.toString();
+  currentOperation = "";
+  previousInput = "";
+  display.textContent = currentInput;
+}
+
+function appendOperation(operation) {
+  if (currentInput === "") return;
+  if (previousInput !== "") {
+    calculate(); // Calculate the previous operation before appending a new one
+  }
+  currentOperation = operation;
+  previousInput = currentInput;
+  currentInput = "";
+  display.textContent = `${previousInput} ${currentOperation}`;
+
+  console.log(previousInput);
+  console.log(currentOperation);
+  console.log(currentInput);
+}
+
+function clearDisplay() {
+  currentInput = "";
+  previousInput = "";
+  currentOperation = "";
+  display.textContent = "0";
+}
