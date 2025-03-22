a314bed43abd2186b1b6dbed4733c4e3f8edbbba
123itismesara <sfalch24@student.aau.dk>
2025-03-22 23:15:06 +0100

    next or prev suit

--- first_website/features/pretties/pretties.js
+++ first_website/features/pretties/pretties.js
@@ -109,19 +109,11 @@ let cards_arr = [
   z1,
 ];
 
-// function showNextImage() {
-//     setImageIndex((index) => {
-//         if (index === imageUrls.length - 1) return 0;
-//         return index + 1;
-//     });
-// }
-
-// function showPrevImage() {
-//     setImageIndex((index) => {
-//         if (index === 0) return imageUrls.length - 1;
-//         return index - 1;
-//     });
-// }
+// array with the suits:
+let hearts = [a, b, c, d, e, f, g, h, i, j, k, l, m];
+let spades = [n, o, p, q, r, s, t, u, v, w, x, y, z];
+let diamond = [a1, b1, c1, d1, e1, f1, g1, h1, i1, j1, k1, l1, m1];
+let clubs = [n1, o1, p1, q1, r1, s1, t1, u1, v1, w1, x1, y1, z1];
 
 let left_id = document.getElementById("left-card");
 let main_id = document.getElementById("main-card");
@@ -135,8 +127,7 @@ let index_r = 1;
 //length of array
 let len = 51;
 
-function goLeft() {
-  //
+function goRight() {
   if (index_r === 51) {
     left_id.src = cards_arr[index_m];
     main_id.src = cards_arr[index_r];
@@ -146,13 +137,13 @@ function goLeft() {
     index_m = index_r;
     index_r = 0;
 
-    console.log("GoL - left: " + left_id.src);
-    console.log("GoL - main: " + main_id.src);
-    console.log("GoL - right: " + right_id.src);
+    // console.log("GoL - left: " + left_id.src);
+    // console.log("GoL - main: " + main_id.src);
+    // console.log("GoL - right: " + right_id.src);
 
-    console.log("INDEX L l" + index_l);
-    console.log("INDEX L m" + index_m);
-    console.log("INDEX L r" + index_r);
+    // console.log("INDEX L l" + index_l);
+    // console.log("INDEX L m" + index_m);
+    // console.log("INDEX L r" + index_r);
   }
   left_id.src = cards_arr[index_m];
   main_id.src = cards_arr[index_r];
@@ -162,17 +153,16 @@ function goLeft() {
   index_m = index_r;
   index_r = index_r + 1;
 
-  console.log("GoL - left: " + left_id.src);
-  console.log("GoL - main: " + main_id.src);
-  console.log("GoL - right: " + right_id.src);
+  //   console.log("GoL - left: " + left_id.src);
+  //   console.log("GoL - main: " + main_id.src);
+  //   console.log("GoL - right: " + right_id.src);
 
-  console.log("INDEX L l" + index_l);
-  console.log("INDEX L m" + index_m);
-  console.log("INDEX L r" + index_r);
+  //   console.log("INDEX L l" + index_l);
+  //   console.log("INDEX L m" + index_m);
+  //   console.log("INDEX L r" + index_r);
 }
 
-function goRight() {
-  //
+function goLeft() {
   if (index_l === 0) {
     right_id.src = cards_arr[index_m];
     main_id.src = cards_arr[index_l];
@@ -182,13 +172,13 @@ function goRight() {
     index_m = index_l;
     index_l = len;
 
-    console.log("GoR - left: " + left_id.src);
-    console.log("GoR - main: " + main_id.src);
-    console.log("GoR - right: " + right_id.src);
+    // console.log("GoR - left: " + left_id.src);
+    // console.log("GoR - main: " + main_id.src);
+    // console.log("GoR - right: " + right_id.src);
 
-    console.log("INDEX R l" + index_l);
-    console.log("INDEX R m" + index_m);
-    console.log("INDEX R r" + index_r);
+    // console.log("INDEX R l" + index_l);
+    // console.log("INDEX R m" + index_m);
+    // console.log("INDEX R r" + index_r);
   }
   right_id.src = cards_arr[index_m];
   main_id.src = cards_arr[index_l];
@@ -198,11 +188,122 @@ function goRight() {
   index_m = index_l;
   index_l = index_l - 1;
 
-  console.log("GoR - left: " + left_id.src);
-  console.log("GoR - main: " + main_id.src);
-  console.log("GoR - right: " + right_id.src);
+  //   console.log("GoR - left: " + left_id.src);
+  //   console.log("GoR - main: " + main_id.src);
+  //   console.log("GoR - right: " + right_id.src);
 
-  console.log("INDEX R l" + index_l);
-  console.log("INDEX R m" + index_m);
-  console.log("INDEX R r" + index_r);
+  //   console.log("INDEX R l" + index_l);
+  //   console.log("INDEX R m" + index_m);
+  //   console.log("INDEX R r" + index_r);
 }
+
+//go to next suit
+function nextSuit() {
+  //if heart, go to next
+  if (index_m <= 12) {
+    right_id.src = cards_arr[14];
+    main_id.src = cards_arr[13];
+    left_id.src = cards_arr[12];
+
+    index_r = 14;
+    index_m = 13;
+    index_l = 12;
+  }
+  //if spades, go to next
+  else if (index_m <= 25) {
+    right_id.src = cards_arr[27];
+    main_id.src = cards_arr[26];
+    left_id.src = cards_arr[25];
+
+    index_r = 27;
+    index_m = 26;
+    index_l = 25;
+  }
+  //if diamonds, go to next
+  else if (index_m <= 38) {
+    right_id.src = cards_arr[40];
+    main_id.src = cards_arr[39];
+    left_id.src = cards_arr[38];
+
+    index_r = 40;
+    index_m = 39;
+    index_l = 38;
+  }
+  //if clubs, go to next
+  else {
+    right_id.src = cards_arr[1];
+    main_id.src = cards_arr[0];
+    left_id.src = cards_arr[51];
+
+    index_r = 1;
+    index_m = 0;
+    index_l = 51;
+  }
+
+  //   console.log("suit: " + left_id.src);
+  //   console.log("suit: " + main_id.src);
+  //   console.log("suit: " + right_id.src);
+
+  //   console.log("suit: " + index_l);
+  //   console.log("suit: " + index_m);
+  //   console.log("suit: " + index_r);
+}
+
+//go to previous suit
+function previousSuit() {
+  //if heart, go to previous
+  if (index_m <= 12) {
+    right_id.src = cards_arr[40];
+    main_id.src = cards_arr[39];
+    left_id.src = cards_arr[38];
+
+    index_r = 40;
+    index_m = 39;
+    index_l = 38;
+  }
+  //if spades, go to previous
+  else if (index_m <= 25) {
+    right_id.src = cards_arr[1];
+    main_id.src = cards_arr[0];
+    left_id.src = cards_arr[51];
+
+    index_r = 1;
+    index_m = 0;
+    index_l = 51;
+  }
+  //if diamonds, go to previous
+  else if (index_m <= 38) {
+    right_id.src = cards_arr[14];
+    main_id.src = cards_arr[13];
+    left_id.src = cards_arr[12];
+
+    index_r = 14;
+    index_m = 13;
+    index_l = 12;
+  }
+  //if clubs, go to previous
+  else {
+    right_id.src = cards_arr[27];
+    main_id.src = cards_arr[26];
+    left_id.src = cards_arr[25];
+
+    index_r = 27;
+    index_m = 26;
+    index_l = 25;
+  }
+}
+
+// //go n forward in card pile
+// function goForward(n) {
+//   right_id.src = cards_arr[index_r + n];
+//   main_id.src = cards_arr[index_m + n];
+//   left_id.src = cards_arr[index_l + n];
+
+//   index_r += n;
+//   index_m += n;
+//   index_l += n;
+
+//   if (index_m === 46) {
+
+//   }
+// }
