78fb26cd04c32a9c6535f0331a657b76872b1b2f
123itismesara <sfalch24@student.aau.dk>
2025-03-23 21:49:39 +0100

    top and bottom of page buttons

--- first_website/main.js
+++ first_website/main.js
@@ -2,3 +2,13 @@
 function refreshPage() {
   window.location.reload();
 }
+
+//https://jsfiddle.net/9bazc6hx/2/
+//scroll to bottom of page
+const scrollToTop = () => {
+  document.documentElement.scrollTop = 0;
+};
+
+const scrollToBottom = () => {
+  document.documentElement.scrollTop = document.documentElement.scrollHeight;
+};
