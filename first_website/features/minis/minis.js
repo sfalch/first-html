16ee166932aa43f0103689670f4e6417ca5ed0e0
123itismesara <sfalch24@student.aau.dk>
2025-03-23 18:54:28 +0100

    iseven API

--- first_website/features/minis/minis.js
+++ first_website/features/minis/minis.js
@@ -347,7 +347,34 @@ async function fetchPokemon() {
     const pokemon_img = data.sprites.front_shiny;
     const imgElement = document.getElementById("pokemon_img");
     imgElement.src = pokemon_img;
-    imgElement.style.display = "block";
+    imgElement.style.display = "inline-block";
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//fetch is even api
+async function fetchisEven() {
+  try {
+    const number = document.getElementById("isEvenInput").value;
+
+    const response = await fetch(
+      `https://api.isevenapi.xyz/api/iseven/${number}/`
+    );
+
+    if (!response.ok) {
+      throw new Error("Could not fetch resource");
+    }
+
+    const data = await response.json();
+    const number_bool = data.iseven;
+    const txtElement = document.getElementById("revealEven");
+
+    if (number_bool === true) {
+      txtElement.textContent = number + " is even!";
+    } else if (number_bool === false) {
+      txtElement.textContent = number + " is odd!";
+    }
   } catch (error) {
     console.log(error);
   }
