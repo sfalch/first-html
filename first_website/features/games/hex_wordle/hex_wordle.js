bac5a3f977831cd0fb527b5dabe4154cb6b62b3f
123itismesara <sfalch24@student.aau.dk>
2025-03-24 22:27:54 +0100

    hex wordle done!

--- first_website/features/games/hex_wordle/hex_wordle.js
+++ first_website/features/games/hex_wordle/hex_wordle.js
@@ -8,7 +8,7 @@ const answer_text = document.getElementById("answer_text");
 let guess_counter = 0;
 
 //array with hex things.
-const hex_arrray = [
+const hex_array = [
   "0",
   "1",
   "2",
@@ -27,16 +27,25 @@ const hex_arrray = [
   "F",
 ];
 
+let hex_object = {
+  r1: "",
+  r2: "",
+  g1: "",
+  g2: "",
+  b1: "",
+  b2: "",
+};
+
 //global variable for answer hex
 let answer_hex = "";
 
 const random_hex = () => {
   let n = Math.floor(Math.random() * 16);
-  return hex_arrray[n];
+  return hex_array[n];
 };
 
 function getColor() {
-  let hex_object = {
+  hex_object = {
     r1: random_hex(),
     r2: random_hex(),
     g1: random_hex(),
@@ -76,17 +85,24 @@ function checkGuess() {
   guess_string = guess_string.toUpperCase();
   console.log(guess_string);
 
+  //------------------IF AT END OF GUESSES
+  if (guess_counter > 6) {
+    revealColor();
+    alert("Out of guesses. The correct color was " + answer_hex);
+    return;
+  }
+
   //--------------------------------------
   //going to validate, but not rn
   //--------------------------------------
 
-  //change the code text
-  let code_id = document.getElementById(`guess${guess_counter}_code`);
-  code_id.textContent = "#" + guess_string;
+  //------------------------COLOR CHANGE -------------------------
 
   //change color of circle
   let color_id = document.getElementById(`guess${guess_counter}_circle`);
   color_id.style.backgroundColor = "#" + guess_string;
+
+  //---------------------INFO FOR COMPARISON---------------------
 
   //make guess into an array of chars to compare
   let guess_array = guess_string.split("");
@@ -94,10 +110,142 @@ function checkGuess() {
   //get id for the rgb closeness string
   let RGB_text = document.getElementById(`guess${guess_counter}_rgb`);
 
+  //make guesses into numbers for comparison
+  let hex_number_arr = {
+    r1: HexToNumber(hex_object.r1),
+    r2: HexToNumber(hex_object.r2),
+    g1: HexToNumber(hex_object.g1),
+    g2: HexToNumber(hex_object.g2),
+    b1: HexToNumber(hex_object.b1),
+    b2: HexToNumber(hex_object.b2),
+  };
+  console.log("hex: " + answer_hex);
+  console.log(hex_number_arr);
+
+  let guess_number_arr = {
+    r1: HexToNumber(guess_array[0]),
+    r2: HexToNumber(guess_array[1]),
+    g1: HexToNumber(guess_array[2]),
+    g2: HexToNumber(guess_array[3]),
+    b1: HexToNumber(guess_array[4]),
+    b2: HexToNumber(guess_array[5]),
+  };
+  console.log("guess: " + guess_string);
+  console.log(guess_number_arr);
+
+  //-----------------------CODE UPDATE----------------------
+  let r1_code = document.getElementById(`r1_${guess_counter}`);
+  let r2_code = document.getElementById(`r2_${guess_counter}`);
+  let g1_code = document.getElementById(`g1_${guess_counter}`);
+  let g2_code = document.getElementById(`g2_${guess_counter}`);
+  let b1_code = document.getElementById(`b1_${guess_counter}`);
+  let b2_code = document.getElementById(`b2_${guess_counter}`);
+
+  //update the text
+  r1_code.textContent = guess_array[0];
+  r2_code.textContent = guess_array[1];
+  g1_code.textContent = guess_array[2];
+  g2_code.textContent = guess_array[3];
+  b1_code.textContent = guess_array[4];
+  b2_code.textContent = guess_array[5];
+
+  //red color update
+  if (guess_number_arr.r1 === hex_number_arr.r1) {
+    r1_code.style.color = "green";
+  }
+  if (guess_number_arr.r2 === hex_number_arr.r2) {
+    r2_code.style.color = "green";
+  }
+
+  //green color update
+  if (guess_number_arr.g1 === hex_number_arr.g1) {
+    g1_code.style.color = "green";
+  }
+  if (guess_number_arr.g2 === hex_number_arr.g2) {
+    g2_code.style.color = "green";
+  }
+
+  //blue color update
+  if (guess_number_arr.b1 === hex_number_arr.b1) {
+    b1_code.style.color = "green";
+  }
+  if (guess_number_arr.b2 === hex_number_arr.b2) {
+    b2_code.style.color = "green";
+  }
+
+  //---------------------------RGB TEXT UPDATE---------------------
+  let red_text = "";
+  let green_text = "";
+  let blue_text = "";
+
+  if (guess_number_arr.r1 === hex_number_arr.r1) {
+    if (guess_number_arr.r2 > hex_number_arr.r2) {
+      red_text = "Too much red";
+    } else if (guess_number_arr.r2 < hex_number_arr.r2) {
+      red_text = "Too little red";
+    } else {
+      red_text = "Red is wrong";
+    }
+  } else if (guess_number_arr.r1 > hex_number_arr.r1) {
+    red_text = "Too much red";
+  } else if (guess_number_arr.r1 < hex_number_arr.r1) {
+    red_text = "Too little red";
+  } else {
+    red_text = "Red is wrong";
+  }
+
+  //compare green
+  if (guess_number_arr.g1 === hex_number_arr.g1) {
+    if (guess_number_arr.g2 > hex_number_arr.g2) {
+      green_text = "Too much green";
+    } else if (guess_number_arr.g2 < hex_number_arr.g2) {
+      green_text = "Too little green";
+    } else {
+      green_text = "Green is wrong";
+    }
+  } else if (guess_number_arr.g1 > hex_number_arr.g1) {
+    green_text = "Too much green";
+  } else if (guess_number_arr.g1 < hex_number_arr.g1) {
+    green_text = "Too little green";
+  } else {
+    green_text = "Green is wrong";
+  }
+
+  //compare blue
+  if (guess_number_arr.b1 === hex_number_arr.b1) {
+    if (guess_number_arr.b2 > hex_number_arr.b2) {
+      blue_text = "Too much blue";
+    } else if (guess_number_arr.b2 < hex_number_arr.b2) {
+      blue_text = "Too little blue";
+    } else {
+      blue_text = "Blue is wrong";
+    }
+  } else if (guess_number_arr.b1 > hex_number_arr.b1) {
+    blue_text = "Too much blue";
+  } else if (guess_number_arr.b1 < hex_number_arr.b1) {
+    blue_text = "Too little blue";
+  } else {
+    blue_text = "Blue is wrong";
+  }
+
+  let rgb_text = `${red_text}, ${green_text}, ${blue_text}`;
+
+  RGB_text.textContent = rgb_text;
+
   //compare if guessed correct
   console.log(answer_hex);
   console.log(`#${guess_string}`);
   if (answer_hex === `#${guess_string}`) {
     RGB_text.textContent = "Correct!";
   }
+}
+
+function HexToNumber(x) {
+  let n = 0;
+  for (let i = 0; i < hex_array.length; i++) {
+    if (x === hex_array[i]) {
+      n = i;
+    }
+  }
+  return n;
 }
